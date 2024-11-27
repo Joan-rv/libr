52b45e4aa13cfaa50ecd64f7fbf4dc15b965c662
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-11-27 13:11:48 +0100

    Update tests

--- test.c
+++ test.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main() {
-    char* fmt = "Hi %c, %d, 50%%\n";
-    int r1 = r_printf(fmt, 'a', -222);
-    int r2 = printf(fmt, 'a', -222);
+    char* fmt = "Hi %c, %d, 50%%, %f\n";
+    int r1 = r_printf(fmt, 'a', -222, -2.2f);
+    int r2 = printf(fmt, 'a', -222, -2.2f);
     printf("r1=%d, r2=%d\n", r1, r2);
 }
