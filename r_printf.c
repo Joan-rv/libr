26d36e0c81755d0d82df6149d30d37dae849fe55
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-11-27 12:10:14 +0100

    Add intiger parsing

--- r_printf.c
+++ r_printf.c
@@ -1,7 +1,40 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// log base 10 of 2
+#define LOG2 0.301029995664
+
+void print_num(int n) {
+    int i = 0;
+    char s[(size_t)(8 * sizeof(int) * LOG2 + 2)];
+    if (n < 0) {
+        n = -n;
+        s[i] = '-';
+        i++;
+    }
+    int p = 1;
+    for (int m = n; m > 9; m /= 10) {
+        p *= 10;
+    }
+
+    for (int q = p; q > 0; q /= 10) {
+        s[i] = (n / q) % 10 + '0';
+        i++;
+    }
+    write(STDOUT_FILENO, s, i);
+}
+
 void arg_parse(const char* restrict* fmt, va_list* args) {
     if ((*fmt)[1] == 'c') {
         char c = va_arg(*args, int);
         write(STDOUT_FILENO, &c, 1);
+        *fmt += 2;
+    }
+
+    if ((*fmt)[1] == 'd') {
+        int i = va_arg(*args, int);
+        print_num(i);
         *fmt += 2;
     }
 }
