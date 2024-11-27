dd2b21474b8111873406c4530af71ac8b5c88912
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-11-27 12:37:09 +0100

    Return number of bytes written or -1 on error

--- r_printf.c
+++ r_printf.c
@@ -1,52 +1,65 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void print_num(int n) {
+int print_num(int n) {
     char c = (n % 10) + '0';
     if (n < 0) {
         n = -n;
         c = '-';
         write(STDOUT_FILENO, &c, 1);
-        print_num(n);
+        return 1 + print_num(n);
     } else if (n < 10) {
         write(STDOUT_FILENO, &c, 1);
+        return 1;
     } else {
-        print_num(n / 10);
+        int m = print_num(n / 10);
         write(STDOUT_FILENO, &c, 1);
+        return 1 + m;
     }
 }
 
-void arg_parse(const char* restrict* fmt, va_list* args) {
+int arg_parse(const char* restrict* fmt, va_list* args) {
     if ((*fmt)[1] == 'c') {
         char c = va_arg(*args, int);
-        write(STDOUT_FILENO, &c, 1);
         *fmt += 2;
+        write(STDOUT_FILENO, &c, 1);
+        return 1;
     } else if ((*fmt)[1] == 'd') {
         int i = va_arg(*args, int);
-        print_num(i);
         *fmt += 2;
+        return print_num(i);
     } else if ((*fmt)[1] == '%') {
         write(STDOUT_FILENO, *fmt, 1);
         *fmt += 2;
+        return 1;
+    } else {
+        return -1;
     }
 }
 
 int r_printf(const char* restrict fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    int i = 0;
+    int i = 0, n = 0;
     while (fmt[i] != '\0') {
         if (fmt[i] == '%') {
             // arg parse
             write(STDOUT_FILENO, fmt, i);
+            n += i;
             fmt += i;
-            arg_parse(&fmt, &args);
+            int m;
+            if ((m = arg_parse(&fmt, &args)) < 0) {
+                return -1;
+            } else {
+                n += m;
+            }
             i = 0;
         } else {
             i++;
         }
     }
     write(STDOUT_FILENO, fmt, i);
+    n += i;
     va_end(args);
-    return 0;
+    return n;
 }
