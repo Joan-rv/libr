713191cb7022092f9355790784ed7c35a1eaabea
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-11-27 13:30:50 +0100

    Add string test

--- test.c
+++ test.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main() {
-    char* fmt = "Hi %c, %d, 50%%, %f\n";
-    int r1 = r_printf(fmt, 'a', -222, -2.2f);
-    int r2 = printf(fmt, 'a', -222, -2.2f);
+    char* fmt = "Hi %c, %d, 50%%, %f, %s\n";
+    int r1 = r_printf(fmt, 'a', -222, -2.2f, "hello");
+    int r2 = printf(fmt, 'a', -222, -2.2f, "hello");
     printf("r1=%d, r2=%d\n", r1, r2);
 }
